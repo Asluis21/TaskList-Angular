786be1b34ff473dceca2ea9b263ae02d317fb11c
luis <luis_alvarado1225@outlook.com>
2023-08-09 23:09:16 -0500

    search-bar corrected
    app-component was changed with task-form and task component(lists)

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit{
 
+  editModeExist: boolean = false;
+
+  constructor(private activeRouter: ActivatedRoute){ }
+
+  ngOnInit(){
+    this.editModeExist = !('editMode' in this.activeRouter.snapshot.data);
+  }
 }
